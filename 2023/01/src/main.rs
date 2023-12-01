0b678049f078c6b5329cff9bf5b0836b2dda0fd4
Suket Arya <suketsarya@gmail.com>
2023-12-01 14:29:02 +0000

    23/2: complete

--- 2023/01/src/main.rs
+++ 2023/01/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc::Input;
 
 aoc::parts!(1, 2);
@@ -29,8 +31,69 @@ fn part_1(input: Input) -> impl ToString {
 
 fn part_2(input: Input) -> impl ToString {
     let mut total = 0;
-    let char_digits = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
+    let digits_forwards = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    for line in input.lines() {
+        let (mut first, mut last) = (0, 0);
+        for i in 0..line.len() {
+            if let Some(c) = line.chars().nth(i).unwrap().to_digit(10) {
+                first = c;
+                break;
+            }
+            if i + 2 < line.len() {
+                if let Some(d) = digits_forwards.get(&line[i..i + 3]) {
+                    first = *d;
+                    break;
+                }
+            }
+            if i + 3 < line.len() {
+                if let Some(d) = digits_forwards.get(&line[i..i + 4]) {
+                    first = *d;
+                    break;
+                }
+            }
+            if i + 4 < line.len() {
+                if let Some(d) = digits_forwards.get(&line[i..i + 5]) {
+                    first = *d;
+                    break;
+                }
+            }
+        }
+
+        for i in (0..line.len()).rev() {
+            if let Some(c) = line.chars().nth(i).unwrap().to_digit(10) {
+                last = c;
+                break;
+            }
+            if i > 2 {
+                if let Some(d) = digits_forwards.get(&line[i - 2..=i]) {
+                    last = *d;
+                    break;
+                }
+            }
+            if i > 3 {
+                if let Some(d) = digits_forwards.get(&line[i - 3..=i]) {
+                    last = *d;
+                    break;
+                }
+            }
+            if i > 4 {
+                if let Some(d) = digits_forwards.get(&line[i - 4..=i]) {
+                    last = *d;
+                    break;
+                }
+            }
+        }
+        total += first * 10 + last;
+    }
     total
 }
