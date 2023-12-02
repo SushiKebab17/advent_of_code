b83f990a95d564bd74ef68ae76892cfaadce2092
SushiKebab17 <suketsarya@gmail.com>
2023-12-02 12:37:59 +0000

    23/2: do qol improvements

--- 2023/02/src/main.rs
+++ 2023/02/src/main.rs
@@ -1,51 +1,42 @@
 use std::collections::HashMap;
 
-use aoc::{Input, Parse};
+use aoc::{Input, IterUnwrap, Parse};
 
 aoc::parts!(1, 2);
 
 fn part_1(input: Input) -> impl ToString {
-    let mut total: u32 = 0;
+    let mut total = 0;
     for line in input {
-        total += solve_pt1(line);
+        let (id, max) = parse(line);
+        total += if max["red"] <= 12 && max["green"] <= 13 && max["blue"] <= 14 {
+            id
+        } else {
+            0
+        };
     }
     total
 }
 
 fn part_2(input: Input) -> impl ToString {
-    let mut total: u32 = 0;
+    let mut total = 0;
     for line in input {
-        total += solve_pt2(line);
+        let (_, max) = parse(line);
+        total += max["red"] * max["green"] * max["blue"];
     }
     total
-}
-
-fn solve_pt1(line: &str) -> u32 {
-    let (id, max) = parse(line);
-    if max["red"] <= 12 && max["green"] <= 13 && max["blue"] <= 14 {
-        id
-    } else {
-        0
-    }
-}
-
-fn solve_pt2(line: &str) -> u32 {
-    let (_, max) = parse(line);
-    max["red"] * max["green"] * max["blue"]
 }
 
 fn parse(line: &str) -> (u32, HashMap<&str, u32>) {
     let mut max = HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
     let mut parser = line.as_parser();
-    let id: u32 = parser.between("Game ", ": ").parse().unwrap();
-    let mut split = parser.rest().split("; ");
-    while let Some(handful) = split.next() {
+    let id: u32 = parser.between("Game ", ": ").parse_uw();
+    for handful in parser.rest().split("; ") {
         let mut split_cubes = handful.split(", ");
         while let Some(cubes) = split_cubes.next() {
-            let mut split_color = cubes.split(" ");
-            let num: u32 = split_color.next().unwrap().parse().unwrap();
-            let color = split_color.next().unwrap();
-            *max.get_mut(color).unwrap() = max[color].max(num);
+            let mut split_colour = cubes.split(" ");
+            let num: u32 = split_colour.next_uw().parse_uw();
+            let colour = split_colour.next_uw();
+            max.insert(colour, max[colour].max(num));
         }
     }
     (id, max)
