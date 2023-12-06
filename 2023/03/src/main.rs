493dd6005223668a1aad0c0fb5c2f9662414cb6f
SushiKebab17 <suketsarya@gmail.com>
2023-12-06 19:19:22 +0000

    23/3: do part 1

--- 2023/03/src/main.rs
+++ 2023/03/src/main.rs
@@ -1,31 +1,37 @@
 use std::collections::HashMap;
 
-use aoc::{Input, Parse};
+use aoc::Input;
 use grid::prelude::*;
 
-aoc::parts!(1);
+aoc::parts!(1, 2);
 
 fn part_1(input: Input) -> impl ToString {
-    let number_positions = parse(input);
-    let lines = input.len();
-    let columns = input[0].len();
+    let mut number_positions = parse(input);
     let mut total = 0;
     for (i, line) in input.lines().enumerate() {
         for (j, ch) in line.chars().enumerate() {
             if ch.to_digit(10) == None && ch != '.' {
-                let pos = v(i as i64, j as i64);
-                let adj = ADJACENT.map(|v| v + pos);
+                let adj = ADJACENT.iter().map(|&vec| vec + v(i as i64, j as i64));
+                for pos in adj {
+                    if number_positions.contains_key(&pos) {
+                        let num_pos = number_positions[&pos];
+                        total += num_pos.num;
+                        for k in (num_pos.low)..(num_pos.high) {
+                            number_positions.remove(&v(num_pos.row, k as i64));
+                        }
+                    }
+                }
             }
         }
     }
-    0
+    total
 }
 
 // fn part_2(input: Input) -> impl ToString {
 //     0
 // }
 
-fn parse(input: Input) -> HashMap<Vector, u32> {
+fn parse(input: Input) -> HashMap<Vector, NumberPos> {
     let mut number_positions = HashMap::new();
     for (i, line) in input.lines().enumerate() {
         let mut num = 0;
@@ -35,16 +41,45 @@ fn parse(input: Input) -> HashMap<Vector, u32> {
                 num = 10 * num + digit;
             } else {
                 for k in first_index_num..j {
-                    // println!("{:?}, {}", v(i as i64, k as i64), num);
-                    number_positions.insert(v(i as i64, k as i64), num);
+                    number_positions.insert(
+                        v(i as i64, k as i64),
+                        NumberPos::new(num, i as i64, first_index_num as i64, j as i64),
+                    );
                 }
                 first_index_num = j + 1;
                 num = 0;
             }
         }
         for k in first_index_num..line.chars().count() {
-            number_positions.insert(v(i as i64, k as i64), num);
+            number_positions.insert(
+                v(i as i64, k as i64),
+                NumberPos::new(
+                    num,
+                    i as i64,
+                    first_index_num as i64,
+                    line.chars().count() as i64,
+                ),
+            );
         }
     }
     number_positions
+}
+
+#[derive(Debug, Clone, Copy)]
+struct NumberPos {
+    num: u32,
+    row: i64,
+    low: i64,
+    high: i64,
+}
+
+impl NumberPos {
+    fn new(num: u32, row: i64, low: i64, high: i64) -> Self {
+        NumberPos {
+            num,
+            row,
+            low,
+            high,
+        }
+    }
 }
