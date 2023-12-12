8ad834a8e2284876f82c648386fd8729527b429b
SushiKebab17 <suketsarya@gmail.com>
2023-12-12 18:37:29 +0000

    23/9: refactor

--- 2023/09/src/main.rs
+++ 2023/09/src/main.rs
@@ -3,36 +3,33 @@ use aoc::{Input, Parse};
 aoc::parts!(1, 2);
 
 fn part_1(input: Input) -> impl ToString {
-    let mut extrapolated_values_sum = 0;
-    for line in input {
-        let mut sequence: Vec<i32> = line.split_whitespace().map(|x| x.parse_uw()).collect();
-        extrapolated_values_sum += get_forwards(&mut sequence);
-    }
-    extrapolated_values_sum
+    solve(input, false)
 }
 
 fn part_2(input: Input) -> impl ToString {
-    let mut extrapolated_values_sum = 0;
-    for line in input {
-        let mut sequence: Vec<i32> = line.split_whitespace().map(|x| x.parse_uw()).collect();
-        sequence.reverse();
-        extrapolated_values_sum += get_forwards(&mut sequence);
-    }
-    extrapolated_values_sum
+    solve(input, true)
 }
 
-fn get_forwards(seq: &mut [i32]) -> i32 {
-    let mut max_index = seq.len() - 1;
-    let mut zeroes = false;
-    while !zeroes {
-        zeroes = true;
-        for i in 0..max_index {
-            seq[i] = seq[i + 1] - seq[i];
-            if seq[i] != 0 {
-                zeroes = false;
-            }
+fn solve(input: Input, is_part_two: bool) -> i32 {
+    let mut extrapolated_values_sum = 0;
+    for line in input {
+        let mut seq: Vec<i32> = line.ints_iter().collect();
+        if is_part_two {
+            seq.reverse();
         }
-        max_index -= 1;
+        let mut max_index = seq.len() - 1;
+        let mut zeroes = false;
+        while !zeroes {
+            zeroes = true;
+            for i in 0..max_index {
+                seq[i] = seq[i + 1] - seq[i];
+                if seq[i] != 0 {
+                    zeroes = false;
+                }
+            }
+            max_index -= 1;
+        }
+        extrapolated_values_sum += seq.iter().sum::<i32>();
     }
-    seq.iter().sum()
+    extrapolated_values_sum
 }
