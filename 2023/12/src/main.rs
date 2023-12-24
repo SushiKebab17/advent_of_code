51e6b70f69aa7eb3135d5e0030c2a1d20a92750b
SushiKebab17 <suketsarya@gmail.com>
2023-12-24 00:41:00 +0000

    23/12: complete

--- 2023/12/src/main.rs
+++ 2023/12/src/main.rs
@@ -1,94 +1,68 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, usize};
 
 use aoc::{Input, Parse};
 
-aoc::parts!(1);
+aoc::parts!(1, 2);
 
 fn part_1(input: Input) -> impl ToString {
     let mut total = 0;
     for line in input {
         let mut parser = line.as_parser();
         let springs = parser.before(" ");
-        let contig_groups: Vec<u32> = parser.rest().split(",").map(|s| s.parse_uw()).collect();
-        // println!(
-        //     "{:?}, {:?}, {}\n",
-        //     springs,
-        //     &contig_groups,
-        //     calculate_arrs(springs, &contig_groups, 0, 0, 0)
-        // );
-        // total += calculate_arrs(springs, &contig_groups, 0, 0, 0);
+        let contig_groups: Vec<u64> = parser.rest().split(",").map(|s| s.parse_uw()).collect();
+        total += calculate_arrs(springs, &contig_groups);
     }
-
-    // let mut parser = input[20].as_parser();
-    // let springs = parser.before(" ");
-    // let contig_groups: Vec<u32> = parser.rest().split(",").map(|s| s.parse_uw()).collect();
-    // println!(
-    //     "{:?}, {:?}, {}\n",
-    //     springs,
-    //     &contig_groups,
-    //     calculate_arrs(springs, &contig_groups, 0, 0, 0)
-    // );
-    // total += calculate_arrs(springs, &contig_groups, 0, 0, 0);
     total
 }
 
-// fn part_2(input: Input) -> impl ToString {
-//     0
-// }
+fn part_2(input: Input) -> impl ToString {
+    let mut total = 0;
+    for line in input {
+        let mut parser = line.as_parser();
+        let springs = parser.before(" ");
+        let contig_groups: Vec<u64> = parser.rest().split(",").map(|s| s.parse_uw()).collect();
+        let mut dup_springs = springs.to_string();
+        let mut dup_contig_groups = contig_groups.clone();
+        for _ in 0..4 {
+            dup_springs += &("?".to_owned() + springs);
+            dup_contig_groups.append(&mut contig_groups.clone());
+        }
+        total += calculate_arrs(&dup_springs, &dup_contig_groups);
+    }
+    total
+}
 
-// fn calculate_arrs(
-//     springs: &str,
-//     contig_groups: &Vec<u32>,
-//     ch_i: usize,
-//     contig_i: usize,
-//     depth: usize,
-// ) -> u32 {
-//     if contig_i >= contig_groups.len() {
-//         return 1;
-//     } else if ch_i >= springs.len() {
-//         return 0;
-//     }
-//     let mut total = 0;
-//     let group_len = contig_groups[contig_i];
-//     let mut curr = 0;
-//     let mut i = ch_i;
-//     while i < springs.len() {
-//         println!(
-//             "{}{}, {}, {}, {}",
-//             " ".repeat(depth),
-//             springs.chars().nth(i).unwrap(),
-//             curr,
-//             i,
-//             contig_i
-//         );
-//         match springs.chars().nth(i).unwrap() {
-//             '?' | '#' => {
-//                 curr += 1;
-//                 if curr == group_len {
-//                     // println!("recurse");
-//                     if let Some('#') = springs.chars().nth(i + 1) {
-//                     } else {
-//                         if let Some('#') = springs.chars().nth(i - group_len as usize) {
-//                         } else {
-//                             total += calculate_arrs(
-//                                 springs,
-//                                 contig_groups,
-//                                 i + 2,
-//                                 contig_i + 1,
-//                                 depth + 1,
-//                             );
-//                         }
-//                     }
-//                     i -= group_len as usize - 1;
-//                     curr = 0;
-//                     println!("{}{}", " ".repeat(depth), total);
-//                 }
-//             }
-//             _ => {
-//                 curr = 0;
-//             }
-//         }
-//         i += 1;
-//     }
-//     total
-// }
+fn calculate_arrs(springs: &str, contig_groups: &[u64]) -> u64 {
+    let mut pos_count = HashMap::from([(0, 1)]);
+    for (cg_i, &group) in contig_groups.iter().enumerate() {
+        let mut new_pos_count = HashMap::new();
+        for (&pos, &count) in &pos_count {
+            for i in pos..(springs.len() - contig_groups[cg_i + 1..].iter().sum::<u64>() as usize
+                + contig_groups[cg_i + 1..].len())
+            {
+                if (i + (group as usize) - 1 < springs.len()
+                    && !springs[i..i + group as usize].contains("."))
+                    && ((cg_i < contig_groups.len() - 1
+                        && i + (group as usize) < springs.len()
+                        && springs.chars().nth(i + group as usize).unwrap() != '#')
+                        || (cg_i == contig_groups.len() - 1
+                            && !springs[i + group as usize..].contains("#")))
+                {
+                    new_pos_count.insert(
+                        i + group as usize + 1,
+                        if let Some(val) = new_pos_count.get(&(i + group as usize + 1)) {
+                            val + count
+                        } else {
+                            count
+                        },
+                    );
+                }
+                if springs.chars().nth(i).unwrap() == '#' {
+                    break;
+                }
+            }
+        }
+        pos_count = new_pos_count.clone();
+    }
+    pos_count.values().sum()
+}
